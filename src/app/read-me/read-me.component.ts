c97a809c83080d88e015574efa59fb6fcfd8f582
Zbigi <zibifank@interia.pl>
2016-12-27 16:29:13 +0100

    update animation example

--- src/app/read-me/read-me.component.ts
+++ src/app/read-me/read-me.component.ts
@@ -122,24 +122,24 @@ import { CircusScrollDirective } from 'your_path_to/node_modules/dist/circus-scr
 
     //..    
 
-anim1OnBeginCallbackFunction(data) {
-    console.log('function: OnBegin', 'element:', data.el);
-}
-
-anim1OnEndCallbackFunction(data) {
-    console.log('function: OnEnd', 'element:', data.el);
-}
-
-anim1OnReverseBeginCallbackFunction(data) {
-    console.log('function: OnReverseBegin', 'element:', data.el);
-}
-
-anim1OnReverseEndCallbackFunction(data) {
-    console.log('function: OnReverseEnd', 'element:', data.el);
-}
-anim1OnProgressCallbackFunction(data) {
-  console.log('function: onProgress', 'element:', data.el, 'progress:', data.progress);
-}
+  anim1OnBeginCallbackFunction(data) {
+      console.log('function: OnBegin', 'element:', data.el);
+  }
+  
+  anim1OnEndCallbackFunction(data) {
+      console.log('function: OnEnd', 'element:', data.el);
+  }
+  
+  anim1OnReverseBeginCallbackFunction(data) {
+      console.log('function: OnReverseBegin', 'element:', data.el);
+  }
+  
+  anim1OnReverseEndCallbackFunction(data) {
+      console.log('function: OnReverseEnd', 'element:', data.el);
+  }
+  anim1OnProgressCallbackFunction(data) {
+    console.log('function: onProgress', 'element:', data.el, 'progress:', data.progress);
+  }
 
   //.. 
 
--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -15,22 +15,22 @@ export class WelcomePageComponent implements OnInit {
   // Callback functions
 
   anim1OnBeginCallbackFunction(data) {
-     console.log('function: OnBegin', 'element:', data.el);
+    //  console.log('function: OnBegin', 'element:', data.el);
   }
 
   anim1OnEndCallbackFunction(data) {
-     console.log('function: OnEnd', 'element:', data.el);
+    //  console.log('function: OnEnd', 'element:', data.el);
   }
 
   anim1OnReverseBeginCallbackFunction(data) {
-     console.log('function: OnReverseBegin', 'element:', data.el);
+    //  console.log('function: OnReverseBegin', 'element:', data.el);
   }
 
   anim1OnReverseEndCallbackFunction(data) {
-     console.log('function: OnReverseEnd', 'element:', data.el);
+    //  console.log('function: OnReverseEnd', 'element:', data.el);
   }
   anim1OnProgressCallbackFunction(data) {
-    console.log('function: onProgress', 'element:', data.el, 'progress:', data.progress);
+    // console.log('function: onProgress', 'element:', data.el, 'progress:', data.progress);
   }
 
 }
